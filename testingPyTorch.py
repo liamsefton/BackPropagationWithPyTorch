84673135e45be167fdc99383e3179c0aa7f129df
liamsefton <88951421+liamsefton@users.noreply.github.com>
2021-12-07 18:45:37 -0800

    Add files via upload

--- testingPyTorch.py
+++ testingPyTorch.py
@@ -10,11 +10,20 @@ class ExampleNet(nn.Module):
         super(ExampleNet, self).__init__()
         #This creates the layers
         self.linear_relu_stack = nn.Sequential( 
-            nn.Linear(11, 10),
-            nn.ReLU(),
-            nn.Linear(10, 7),
-            nn.ReLU(),
-            nn.Linear(7, 1)
+            nn.Linear(19, 20),
+            #nn.ReLU(),
+            nn.Tanh(),
+            nn.Linear(20, 15),
+            #nn.ReLU(),
+            nn.Tanh(),
+            nn.Linear(15, 10),
+            #nn.ReLU(),
+            nn.Tanh(),
+            nn.Linear(10, 5),
+            #nn.ReLU(),
+            nn.Tanh(),
+            nn.Linear(5, 1)
+            #nn.Tanh()
         )
 
     def forward(self, x):
@@ -22,14 +31,17 @@ class ExampleNet(nn.Module):
         return self.linear_relu_stack(x)
 
 #this function is used to initialize the weights to a set value to reduce randomness
+line_num = 1
 def init_weights(layer):
     if type(layer) == nn.Linear:
-        layer.weight.data.fill_(0.0)
+        #layer.weight.data.fill_(0.0)
+        layer.bias.data.fill_(1.0)
+
 
 model = ExampleNet()
 #model.apply(init_weights)
-num_epochs = 1000
-optimizer = optim.SGD(model.parameters(), lr=.00001) #learning rates higher than this tend to converge to local minima after first epoch
+num_epochs = 5000
+optimizer = optim.Adam(model.parameters(), lr=.00001) #learning rates higher than this tend to converge to local minima after first epoch
 testing_differences = -1
 curr_epoch = 0
 
@@ -43,10 +55,11 @@ convergence_counter = 0
 divergence_counter = 0
 
 plt.xlim(1, 50)
-plt.ylim(0, 8)
+plt.ylim(0, .5)
 plt.title("Error on validation set")
 plt.xlabel("Epochs")
 plt.ylabel("Average error on samples")
+
 
 #Training
 for outer_loop in range(num_epochs):
@@ -54,84 +67,93 @@ for outer_loop in range(num_epochs):
         #for group in optimizer.param_groups:
         #    group['lr'] = group['lr'] - (group['lr']/10) #lowers weights slowly over time
         print(outer_loop)
-    for group in optimizer.param_groups:
-        group['lr'] = group['lr'] - (group['lr']/100)
-    f = open("training.csv")
-    f.readline()
-    sum_differences = 0
+    #for group in optimizer.param_groups:
+    #    group['lr'] = group['lr'] - (group['lr']/100)
+    f = open("training_no_nans.txt")
+    sum_correct = 0
     num_samples = 0
+    line_num = 1
     for line in f:
-        line = line.split(";")
+        line = line.split(",")
         line = list(map(float, line))
         target = [line[-1]] #target must be in list in order to be converted to tensor
-        target = torch.FloatTensor(target) 
+        target = torch.FloatTensor(target)
         line = line[:-1]
-        #line = [float(i)/max(line) for i in line] #normalize the data
+        #output = model(torch.nan_to_num(torch.FloatTensor(line)))
         output = model(torch.FloatTensor(line))
+        #print(output)
+        #print(torch.nan_to_num(output))
         optimizer.zero_grad()
         loss_func = nn.MSELoss()
         loss = loss_func(output, target)
         loss.backward()
         optimizer.step()
-        sum_differences += abs(float(target.item() - output))
+        sum_correct += abs(target - output)
         num_samples += 1
-    training_error_y_vals.append(float(sum_differences)/num_samples)
+        line_num += 1
+
+    training_error_y_vals.append(float(sum_correct)/num_samples)
     x_vals_training.append(curr_epoch)
 
     #Overfitting prevention here
-    divergence_threshold = .001 #if (new average error) - (prev average error) > divergence_threshold
-    convergence_threshold = .0005 #if abs(new average error - prev average error) < convergence_threshold 
-    f = open("validation.csv")
-    f.readline()
-    sum_differences = 0
+    divergence_threshold = .00001 #if (new average error) - (prev average error) > divergence_threshold
+    convergence_threshold = .00005 #if abs(new average error - prev average error) < convergence_threshold 
+    f = open("testing_no_nans.txt")
+    sum_correct = 0
     num_samples = 0
     for line in f:
-        line = line.split(";")
+        line = line.split(",")
         line = list(map(float, line))
         target = [line[-1]]
-        target = torch.FloatTensor(target)
+        target = torch.nan_to_num(torch.FloatTensor(target))
         line = line[:-1]
         #line = [float(i)/max(line) for i in line] 
-        output = model(torch.FloatTensor(line))
-        sum_differences += abs(float(target.item() - output))
+        output = model(torch.nan_to_num(torch.FloatTensor(line)))
+        print(output)
+        sum_correct += abs(target - output)
         num_samples += 1
     exit_code = -1
-
     #checking for convergence and divergence
     if testing_differences == -1: #if it is first iteration
-        testing_differences = float(sum_differences)/num_samples
-    elif float(sum_differences)/num_samples <= testing_differences: 
+        testing_differences = float(sum_correct)/num_samples
+    elif float(sum_correct)/num_samples <= testing_differences: 
         divergence_counter = 0
-        if testing_differences - float(sum_differences)/num_samples < convergence_threshold:
+        if abs(testing_differences - float(sum_correct)/num_samples) < convergence_threshold:
             convergence_counter += 1
             if convergence_counter == 10:
-                exit_code = 1
+                exit_code = 0
                 break
         else:
             convergence_counter = 0
-            testing_differences = float(sum_differences)/num_samples
+            testing_differences = float(sum_correct)/num_samples
     else:
-        if float(sum_differences)/num_samples - testing_differences > divergence_threshold:
+        if float(sum_correct)/num_samples - testing_differences > divergence_threshold:
             divergence_counter += 1
             if divergence_counter == 5:
-                exit_code = 0
+                exit_code = 1
                 break
         else:
             divergence_counter = 0
 
     curr_epoch += 1
+    line_num += 1
 
-    testing_error_y_vals.append(float(sum_differences)/num_samples)
+    testing_error_y_vals.append(float(sum_correct)/num_samples)
     x_vals_testing.append(curr_epoch)
 
-    if(max(x_vals_testing) > 49):
-        plt.xlim(1, max(x_vals_testing) + 10)
-    if(float(sum_differences)/num_samples > 1):
-        plt.ylim(0,float(sum_differences)/num_samples * 1.5)
-    else:
-        plt.ylim(float(sum_differences)/num_samples * float(sum_differences)/num_samples,float(sum_differences)/num_samples * 1.5)
+
+
+    ylim_multiplier = 2
+    if(max(x_vals_testing) > 39):
+        plt.xlim(1, max(x_vals_testing) + curr_epoch*.75)
+    if(float(sum_correct)/num_samples >= .8):
+        plt.ylim(0,float(sum_correct)/num_samples * ylim_multiplier)
+    elif(float(sum_correct)/num_samples < .8):
+        plt.ylim(float(sum_correct)/num_samples * float(sum_correct)/num_samples,.5)
     plt.plot(x_vals_testing, testing_error_y_vals)
+    ylim_multiplier -= .01
     plt.pause(.01)
+
 
 if exit_code == -1:
     print("Training stopped after " + str(curr_epoch) + " epochs from reaching max epochs.")
@@ -143,24 +165,31 @@ elif exit_code == 1:
 f.close()
 
 #Testing
-f = open("testing.csv")
+f = open("binary_testing_no_nans.txt")
 f.readline()
-sum_differences = 0
+sum_correct = 0
 num_samples = 0
 for line in f:
-    line = line.split(";")
+    line = line.split(",")
     line = list(map(float, line))
     target = [line[-1]]
-    target = torch.FloatTensor(target)
+    target = torch.nan_to_num(torch.FloatTensor(target))
     line = line[:-1]
     #line = [float(i)/max(line) for i in line] #this does normalization
-    output = model(torch.FloatTensor(line))
-    sum_differences += abs(float(target.item() - output))
-    num_samples += 1
+    output = model(torch.nan_to_num(torch.FloatTensor(line)))
+    #print(output)
+    if float(output) > .1:
+        if float(target) == 1:
+            sum_correct += 1
+        num_samples += 1
+    
 print("Testing result: ", end="")
-print(float(sum_differences)/num_samples)
+print(float(sum_correct)/num_samples)
 
 #Look at this graph
+
+#plt.show()
+plt.close()
 plt.plot(x_vals_testing, testing_error_y_vals)
 plt.savefig("testing_differences_by_epoch.png")
 plt.close()
